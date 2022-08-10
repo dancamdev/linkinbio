9497325b0671ccf5a3a2e288340a34645337226e
Daniele Cambi <dancam.dev@gmail.com>
2022-08-10 18:20:52 +0200

    added on page view

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import {logPageView} from "../google-analytics";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => logPageView(url);
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
